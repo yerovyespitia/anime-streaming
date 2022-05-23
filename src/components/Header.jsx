45510ae622218ce971246a659ad090a2d9f22902
yerovyespitia <luyeesgar@gmail.com>
2022-05-22 21:55:52 -0500

    Update react player for ios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,30 +2,9 @@ import React, { useEffect } from "react"
 import styled from "styled-components"
 import ReactPlayer from "react-player"
 import video from "../img/header/video.mp4"
-import wallpaper from "../img/header/wallpaper.png"
 
 const Container = styled.div`
   width: 100%;
-  position: relative;
-`
-
-const VideoContainer = styled.div`
-  position: relative;
-  @media only screen and (max-device-width: 730px) {
-    display: none;
-  }
-`
-
-const Wallpaper = styled.img`
-  width: 100%;
-  height: 100%;
-  display: none;
-  @media only screen and (max-device-width: 730px) {
-    display: inline-block;
-    border-radius: 0 0 16px 16px;
-    height: 65vh;
-    object-fit: cover;
-  }
 `
 
 const Header = () => {
@@ -35,17 +14,15 @@ const Header = () => {
 
   return (
     <Container>
-      <VideoContainer>
-        <ReactPlayer
-          url={video}
-          playing={true}
-          muted={true}
-          width="100%"
-          height="100%"
-          loop={true}
-        />
-      </VideoContainer>
-      <Wallpaper src={wallpaper} />
+      <ReactPlayer
+        url={video}
+        playing={true}
+        muted={true}
+        width="100%"
+        height="100%"
+        loop={true}
+        playsInline={true}
+      />
     </Container>
   )
 }
