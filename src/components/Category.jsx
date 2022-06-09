72419d7ad8527bebde40c92f04568a504a3879a0
yerovyespitia <luyeesgar@gmail.com>
2022-06-08 19:40:53 -0500

    Add info icon

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,10 +1,14 @@
 import React from "react"
 import "../styles/categories.css"
+import info from "../img/info.svg"
 
 const Category = ({ item }) => {
   return (
     <a className="categoryLink" href={`/view/${item.title}`}>
       <img className="categoryImg" src={item.img} alt={item.title} />
+      <div className="infoContainer">
+        <img className="infoIcon" src={info} alt="info" />
+      </div>
     </a>
   )
 }
