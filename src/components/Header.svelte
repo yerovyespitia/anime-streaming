76b9f37ad27b6121cb70bffac8694fab6cd9e637
yerovyespitia <luyeesgar@gmail.com>
2022-09-10 22:25:26 -0500

    Change image links :alien:

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,7 @@
 <div class="relative mb-4">
   <video
     class="w-full h-full opacity-70"
-    src="https://github.com/yerovyespitia/anime-streaming/blob/main/static/assets/video.mp4?raw=true"
+    src="https://github.com/yerovyespitia/anime-streaming-svelte/blob/main/static/assets/video.mp4"
     autoplay
     loop
     muted
@@ -12,8 +12,8 @@
     <a href="/watch/Demon Slayer">
       <img
         class="w-[70%]"
-        src="https://github.com/yerovyespitia/anime-streaming/blob/main/static/assets/logo.png?raw=true"
-        alt="demon slayer logo"
+        src="https://github.com/yerovyespitia/anime-streaming-svelte/blob/main/static/assets/logo.png?raw=true"
+        alt="Demon Slayer Logo"
       />
     </a>
     <p class="font-hel text-xl font-bold text-white mt-5 mb-0 lg:text-2xl">
--- src/data/data.ts
+++ src/data/data.ts
@@ -7,37 +7,37 @@ type Anime = {
 export const populars: Anime[] = [
   {
     title: "Attack on Titan",
-    img: "https://github.com/yerovyespitia/anime-streaming/blob/main/static/assets/thumbnails/aot.png?raw=true",
+    img: "https://github.com/yerovyespitia/anime-streaming-svelte/blob/main/static/assets/thumbnails/aot.png?raw=true",
     id: 0,
   },
   {
     title: "Black Clover",
-    img: "https://github.com/yerovyespitia/anime-streaming/blob/main/static/assets/thumbnails/bc.png?raw=true",
+    img: "https://github.com/yerovyespitia/anime-streaming-svelte/blob/main/static/assets/thumbnails/bc.png?raw=true",
     id: 1,
   },
   {
     title: "Violet Evergarden",
-    img: "https://github.com/yerovyespitia/anime-streaming/blob/main/static/assets/thumbnails/ve.png?raw=true",
+    img: "https://github.com/yerovyespitia/anime-streaming-svelte/blob/main/static/assets/thumbnails/ve.png?raw=true",
     id: 2,
   },
   {
     title: "Serial Experiments Lain",
-    img: "https://github.com/yerovyespitia/anime-streaming/blob/main/static/assets/thumbnails/lain.png?raw=true",
+    img: "https://github.com/yerovyespitia/anime-streaming-svelte/blob/main/static/assets/thumbnails/lain.png?raw=true",
     id: 3,
   },
   {
     title: "Kakegurui",
-    img: "https://github.com/yerovyespitia/anime-streaming/blob/main/static/assets/thumbnails/kakegurui.png?raw=true",
+    img: "https://github.com/yerovyespitia/anime-streaming-svelte/blob/main/static/assets/thumbnails/kakegurui.png?raw=true",
     id: 4,
   },
   {
     title: "Death Note",
-    img: "https://github.com/yerovyespitia/anime-streaming/blob/main/static/assets/thumbnails/deathnote.png?raw=true",
+    img: "https://github.com/yerovyespitia/anime-streaming-svelte/blob/main/static/assets/thumbnails/deathnote.png?raw=true",
     id: 5,
   },
   {
     title: "Demon Slayer",
-    img: "https://github.com/yerovyespitia/anime-streaming/blob/main/static/assets/thumbnails/ds.png?raw=true",
+    img: "https://github.com/yerovyespitia/anime-streaming-svelte/blob/main/static/assets/thumbnails/ds.png?raw=true",
     id: 6,
   },
 ]
